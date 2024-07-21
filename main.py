8a5f01b5d0d9e6a7900813756fc01591370b397f
mrtera <Moritz.wiggert@gmail.com>
2024-07-21 23:27:19 +0200

    verbose

--- main.py
+++ main.py
@@ -134,61 +134,64 @@ class App:
         self.root = root
         self.root.title('Image Processing')
 
+        label = Label(root, text='Upsampling factor for 3D GPU processing is fixed at 2')
+        label.grid(row=0, column=0, columnspan=4)
+
         label = Label(root, text='Upsampleing factor X:')
-        label.grid(row=0, column=1, columnspan=2)
+        label.grid(row=1, column=1, columnspan=2)
         self.upsampling_factor_X_spinbox = Spinbox(root, from_=1, to=100, width=4)
         self.upsampling_factor_X_spinbox.set(2)
-        self.upsampling_factor_X_spinbox.grid(row=0, column=3)
+        self.upsampling_factor_X_spinbox.grid(row=1, column=3)
 
         label = Label(root, text='Upsampleing factor Y:')
-        label.grid(row=1, column=1, columnspan=2)
+        label.grid(row=2, column=1, columnspan=2)
         self.upsampling_factor_Y_spinbox = Spinbox(root, from_=1, to=100, width=4)
         self.upsampling_factor_Y_spinbox.set(2)
-        self.upsampling_factor_Y_spinbox.grid(row=1, column=3)
+        self.upsampling_factor_Y_spinbox.grid(row=2, column=3)
         
         label = Label(root, text='Upsampleing factor Z:')
-        label.grid(row=2, column=1, columnspan=2)
+        label.grid(row=3, column=1, columnspan=2)
         self.upsampling_factor_Z_spinbox = Spinbox(root, from_=1, to=100, width=4)
         self.upsampling_factor_Z_spinbox.set(2)
-        self.upsampling_factor_Z_spinbox.grid(row=2, column=3)
+        self.upsampling_factor_Z_spinbox.grid(row=3, column=3)
 
         self.snow_threshold_spinbox = Spinbox(root, from_=0, to=0.99, width=4, increment=0.1, format='%.2f')
         self.snow_threshold_spinbox.set(0.9)
-        self.snow_threshold_spinbox.grid(row=3, column=3)
+        self.snow_threshold_spinbox.grid(row=4, column=3)
 
         self.remove_snow = BooleanVar(value=True)
         remove_snow_checkbox = Checkbutton(root, text='remove snow above x*max', variable=self.remove_snow)
-        remove_snow_checkbox.grid(row=3, column=1, columnspan=2)
+        remove_snow_checkbox.grid(row=4, column=1, columnspan=2)
 
         self.is2D_video = BooleanVar(value=False)
         is2D_video_checkbox = Checkbutton(root, text='2D Video', variable=self.is2D_video)
-        is2D_video_checkbox.grid(row=3, column=0,)
+        is2D_video_checkbox.grid(row=4, column=0,)
 
         self.do_x_correction = BooleanVar(value=False)
         do_x_correction_checkbox = Checkbutton(root, text='X', variable=self.do_x_correction)
-        do_x_correction_checkbox.grid(row=0, column=0)
+        do_x_correction_checkbox.grid(row=1, column=0)
 
         self.do_y_correction = BooleanVar(value=True)
         do_y_correction_checkbox = Checkbutton(root, text='Y', variable=self.do_y_correction)
-        do_y_correction_checkbox.grid(row=1, column=0)
+        do_y_correction_checkbox.grid(row=2, column=0)
 
         self.do_z_correction = BooleanVar(value=True)
         do_z_correction_checkbox = Checkbutton(root, text='Z', variable=self.do_z_correction)
-        do_z_correction_checkbox.grid(row=2, column=0)
+        do_z_correction_checkbox.grid(row=3, column=0)
 
         self.try_GPU = BooleanVar(value=True)
         try_GPU_checkbox = Checkbutton(root, text='Try GPU', variable=self.try_GPU)
-        try_GPU_checkbox.grid(row=5, column=1)
+        try_GPU_checkbox.grid(row=6, column=1)
         
         self.rescale_image = BooleanVar(value=True)
         rescale_image_checkbox = Checkbutton(root, text='rescale image', variable=self.rescale_image)
-        rescale_image_checkbox.grid(row=5, column=2)
+        rescale_image_checkbox.grid(row=6, column=2)
 
         open = Button(root, text='Open Image', command=self.open_image)
-        open.grid(row=5, column=0, columnspan=1)
+        open.grid(row=6, column=0, columnspan=1)
         
         process = Button(root, text='Process Image', command=self.process)
-        process.grid(row=5, column=3)
+        process.grid(row=6, column=3)
 
 
     def open_image(self):
