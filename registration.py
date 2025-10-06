00ee6bdddf7736782e3383ac5941ed7ee8d35f65
mrtera <moritz.wiggert@gmail.com>
2025-10-06 15:09:02 +0200

    ...

--- registration.py
+++ registration.py
@@ -5,7 +5,7 @@ import itk
 import tifffile as tiff
 import numpy as np
 #read images
-
+#%%
 fixed_image_path = filedialog.askopenfilename(title="Select fixed image")
 moving_image_path = filedialog.askopenfilename(title="Select moving image")
 stack_path = filedialog.askopenfilename(title="Select stack to be transformed")
@@ -16,11 +16,25 @@ def register(fixed_image_path, moving_image_path):
     moving_image = itk.imread(moving_image_path)
     #set up parameters
     parameters = itk.ParameterObject.New()
-    translation_params = parameters.GetDefaultParameterMap("rigid")
-    translation_params["ResultImageFormat"] = ["tiff"]
-    translation_params["ResampleInterpolator"] = ["FinalNearestNeighborInterpolator"]
-    translation_params["MaximumNumberOfIterations"] = ["2000", "2000", "2000"]  # Set to a reasonable number of iterations 2000 for good
-    parameters.AddParameterMap(translation_params)
+    rigid_params = parameters.GetDefaultParameterMap("rigid")
+    affine_params = parameters.GetDefaultParameterMap("affine")
+    bspline_params = parameters.GetDefaultParameterMap("bspline")
+
+    # for key in bspline_params:
+    #     print(key, bspline_params[key])
+    #Modify some
+    rigid_params["ResultImageFormat"] = ["tiff"]
+    rigid_params["resolution"] = ["1", "0.255", "0.45"]
+    affine_params["resolution"] = ["1", "0.255", "0.45"]
+    bspline_params["ResultImageFormat"] = ["tiff"]
+    bspline_params["resolution"] = ["1", "0.255", "0.45"]
+    rigid_params["ResampleInterpolator"] = ["FinalNearestNeighborInterpolator"]
+    affine_params["ResampleInterpolator"] = ["FinalNearestNeighborInterpolator"]
+    bspline_params["FinalBSplineInterpolationOrder"] = ["3"]
+    bspline_params["MaximumNumberOfIterations"] = ["200"]  # Set to a reasonable number of iterations 2000 for good
+    parameters.AddParameterMap(rigid_params)
+    parameters.AddParameterMap(affine_params)
+    # parameters.AddParameterMap(bspline_params)
     print(" image shape: ", fixed_image.shape)
     #Perform Registration
     registered_image, result_parameters = itk.elastix_registration_method(
@@ -34,8 +48,8 @@ def transform_stack(stack_path, result_parameters):
     with tiff.TiffFile(stack_path) as tif:
         np_stack = tif.asarray()
         print("stack shape: ", np_stack.shape)
-    
-    for i in range(int(np_stack.shape[0]/2)):
+
+    for i in range(np.ceil((np_stack.shape[0]/2)).astype(int)):
         volume = itk.GetImageFromArray(np_stack[i*2,:,:,:])
         transformed_slice = itk.transformix_filter(volume, result_parameters)
         np_stack[i*2, :, :, :] = itk.GetArrayFromImage(transformed_slice)
@@ -45,9 +59,10 @@ def transform_stack(stack_path, result_parameters):
 result_image, result_parameters = register(fixed_image_path, moving_image_path)
 #%%
 transformed_stack = transform_stack(stack_path, result_parameters)
+transformed_stack[transformed_stack < 4] = 0
 #%%
 
-tiff.imwrite("transformed_stack.ome.tif",
+tiff.imwrite(stack_path+"transformed_stack.ome.tif",
             transformed_stack,
             photometric='minisblack',
             metadata={'axes': 'TZYX'},
