c7419e8908f1c9aec4ba345b2d5404f057dc63d1
mrtera <moritz.Wiggert@gmail.com>
2025-10-16 11:26:54 +0200

    now is working in parallel

--- HM3D.py
+++ HM3D.py
@@ -9,15 +9,15 @@ from numba import jit, prange
 def median(arr):
 	arr_sorted = np.sort(arr).astype(np.uint16)
 	length = len(arr_sorted)
-	# if length % 2 == 0:
-	# 	median_value = arr_sorted[int(length/2-1)]/2+arr_sorted[int(length/2)]/2
-	# else:
-	# 	median_value = arr_sorted[int((length - 1) / 2)]
-
 	if length % 2 == 0:
-		median_value = (arr_sorted[int(length/2-1)]+arr_sorted[int(length/2)])/2
+		median_value = int(arr_sorted[int(length/2-1)]/2+arr_sorted[int(length/2)]/2)
 	else:
-		median_value = arr_sorted[int((length-1)/2)]
+		median_value = int(arr_sorted[int((length - 1) / 2)])
+
+	# if length % 2 == 0:
+	# 	median_value = (arr_sorted[int(length/2-1)]+arr_sorted[int(length/2)])/2
+	# else:
+	# 	median_value = arr_sorted[int((length-1)/2)]
 	
 	return median_value
 
@@ -25,27 +25,29 @@ def median(arr):
 @jit(nopython=True)
 def get_pixel(stack,z_, y_, x_):
 	# ensure indices are integers and clamp to valid ranges
+
+	if z_ < 0:
+		z_ = 0
+	elif z_ > stack.shape[0] - 1:
+		z_ = stack.shape[0] - 1
+
+	if y_ < 0:
+		y_ = 0
+	elif y_ > stack.shape[1] - 1:
+		y_ = stack.shape[1] - 1
+
+	if x_ < 0:
+		x_ = 0
+	elif x_ > stack.shape[2] - 1:
+		x_ = stack.shape[2] - 1
+
 	zi = int(z_)
-	if zi < 0:
-		zi = 0
-	elif zi > stack.shape[0] - 1:
-		zi = stack.shape[0] - 1
-
 	yi = int(y_)
-	if yi < 0:
-		yi = 0
-	elif yi > stack.shape[1] - 1:
-		yi = stack.shape[1] - 1
-
-	xi = int(x_)
-	if xi < 0:
-		xi = 0
-	elif xi > stack.shape[2] - 1:
-		xi = stack.shape[2] - 1
+	xi = int(x_)	
 
 	return stack[zi, yi, xi]
 	
-@jit(nopython=True)
+@jit(nopython=True, parallel=True)
 def hybrid_3d_median_filter(stack, include_center_pixel=False):
 	stack = stack.astype(np.uint16)  # ensure input is uint16
 	# stack: 3D numpy array (depth, height, width)
@@ -112,12 +114,11 @@ def hybrid_3d_median_filter(stack, include_center_pixel=False):
 				marray3Xd[4] = get_pixel(stack, after_z, y, x - 1)
 
 				# prepare medianarray (7 entries, optionally 8)
-				if include_center_pixel:
-					medianarray = np.zeros(8, dtype=np.uint16)
-					medianarray[7] = get_pixel(stack, z, y, x)
-				else:
-					medianarray = np.zeros(7, dtype=np.uint16)
-					# print(median(marraythisP).dtype, type(median(marraythisP)))	
+				# if include_center_pixel:
+				# 	medianarray = np.zeros(8, dtype=np.uint16)
+				# 	medianarray[7] = get_pixel(stack, z, y, x)
+				# else:
+				medianarray = np.zeros(7, dtype=np.uint16)
 				medianarray[0] = median(marraythisX)
 				medianarray[1] = median(marraythisP)
 				medianarray[2] = median(marray3P)
@@ -125,15 +126,16 @@ def hybrid_3d_median_filter(stack, include_center_pixel=False):
 				medianarray[4] = median(marray3Xb)
 				medianarray[5] = median(marray3Xc)
 				medianarray[6] = median(marray3Xd)
+
 				filtered_stack[z, y, x] = median(medianarray)
 	return filtered_stack
 
 #%%
-filepath = filedialog.askopenfilename()
-with tiff.TiffFile(filepath) as tif:
-	print('Reading image...')
-	image = tif.asarray()
-	print('Image shape:', image.shape)
+# filepath = filedialog.askopenfilename()
+# with tiff.TiffFile(filepath) as tif:
+# 	print('Reading image...')
+# 	image = tif.asarray()
+# 	print('Image shape:', image.shape)
 #%%
 if len(image.shape) == 3:
 	filtered_stack = hybrid_3d_median_filter(image)
